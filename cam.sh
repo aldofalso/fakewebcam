6829258e84ef88979a6f675bb41f7bd5126f28ac
aldofalso <aldo.falso@outlook.com>
2020-11-06 13:46:04 +0100

    add name

--- cam.sh
+++ cam.sh
@@ -2,7 +2,7 @@
 
 #example ./cam.sh video.webm /dev/video2
 
-sudo modprobe v4l2loopback exclusive_caps=1 
+sudo modprobe v4l2loopback devices=1 max_buffers=2 card_label="fakewebcam" exclusive_caps=1 
 
 INPUT=$1
 DEVICE_WEBCAM=$2
@@ -12,6 +12,6 @@ while [ true ]; do
    if [ $? = 0 ]; then
       exit;
    else
-	ffmpeg -v quiet -re -i $INPUT -map 0:v -f v4l2 $DEVICE_WEBCAM
+	ffmpeg -v quiet -re -i $INPUT -map 0:v -f v4l2 $DEVICE_WEBCAM	
    fi
 done
