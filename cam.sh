c3efbfdd5d54e900c857d3c06cc56131422447e1
aldofalso <aldo.falso@outlook.com>
2020-11-25 22:57:17 +0100

    simplify script

--- cam.sh
+++ cam.sh
@@ -5,13 +5,12 @@
 sudo modprobe v4l2loopback devices=1 video_nr=10 max_buffers=2 card_label="fakewebcam" exclusive_caps=1 
 
 INPUT=$1
-DEVICE_WEBCAM=$2
 
 while [ true ]; do
    read -t 1 -n 1
    if [ $? = 0 ]; then
       exit;
    else
-	ffmpeg -re -stream_loop -1 -hwaccel vulkan -i $INPUT -f v4l2 -pix_fmt yuv420p $DEVICE_WEBCAM
+	ffmpeg -re -stream_loop -1 -hwaccel vulkan -i $INPUT -f v4l2 -pix_fmt yuv420p /dev/video10
    fi
 done
